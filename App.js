4398256d699b9df98dcc360c66bc5802f106f01b
Roberto <Robert_jhs@hotmail.com>
2023-03-02 20:31:22 -0600

    Splashscreen created

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
+import { useState, useCallback, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setAppIsLoaded(true), 2000);
+    });
+
+    const onLayout = useCallback(async () => {
+      if (appIsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+    }, [appIsLoaded]);
+
+    if (!appIsLoaded) {
+      return null;
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} onLayout={onLayout}>
             <Text>Hi everyone!</Text>
             <StatusBar style="auto" />
         </View>
