ac739e29d22ce2242300c482292f826122f3f528
Roberto <Robert_jhs@hotmail.com>
2023-03-09 10:58:18 -0600

    Navigation into separated files, auth screens already created

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./MainNavigator";
+import AuthNavigator from "./AuthNavigator";
 
 const AppNavigator = () => {
+
+    const [isLogged, setIsLogged ] = useState(false);
+
     return (
         <NavigationContainer>
             <MainNavigator />
